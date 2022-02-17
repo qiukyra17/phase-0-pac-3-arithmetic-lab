d3876eaf4e37f1c4cf0855b98c9d46cf909cc9fa
kyra qiu <kyraqiu@gmail.com>
2022-02-17 17:53:56 -0500

    commit tree 6797d7940cacef4bd42d0f20228b603c26300537

---
+++ index.js
@@ -0,0 +1,36 @@
+1 + 8;
+60 - 40;
+2* 3.4;
+5.0 / 2.5;
+
+function add(x,y){
+    return x + y; 
+}
+
+function subtract(x,y){
+    return x - y;
+}
+
+function multiply(x,y){
+    return x*y;
+}
+
+function divide(x,y) {
+    return x / y;
+}
+
+function increment(number) {
+    return number += 1;
+}
+
+function decrement(number) {
+    return number -= 1;
+}
+
+function makeInt(string){
+    return parseInt (string, 10);
+}
+
+function preserveDecimal(string) {
+    return parseFloat(string);
+}
